8eacf1c2e63c2a707e39b5cb9a6b84eec88c52b3
Andre Phosarath <andrepho22@gmail.com>
2024-06-08 10:53:41 -0500

    added promises for langtech and vocabulary in API folder

---
+++ api/langTechData.js
@@ -0,0 +1,105 @@
+import client from '../utils/client';
+
+const endpoint = client.databaseURL;
+
+//  GET ALL LanguageTech
+const getLanguageTech = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/languagetech.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }, // you technically do not need the options object for GET requests, but using it here for consistency
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+// CREATE LanguageTech
+const createLanguageTech = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/languagetech.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    }, // you technically do not need the options object for GET requests, but using it here for consistency
+    body: JSON.stringify(payload)
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// GET SINGLE LanguageTech
+const getSingleLanguageTech = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/languagetech/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }, // you technically do not need the options object for GET requests, but using it here for consistency
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data)) // will resolve a single object
+    .catch(reject);
+});
+
+// DELETE LanguageTech
+const deleteSingleLanguageTech = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/languagetech/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    }, // you technically do not need the options object for GET requests, but using it here for consistency
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// UPDATE LanguageTech
+const updateLanguageTech = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/languagetech/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    }, // you technically do not need the options object for GET requests, but using it here for consistency
+    body: JSON.stringify(payload)
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// TODO: GET A SINGLE LanguageTech's VOCABULARY
+const getLanguageTechVocabulary = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary.json?orderBy="languageTechId"&equalTo="${firebaseKey}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// TRENDING THE LanguageTech
+const getTrendingLanguageTech = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/languagetech.json?orderBy="trending"&equalTo=true`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+export {
+  getLanguageTech,
+  createLanguageTech,
+  getSingleLanguageTech,
+  deleteSingleLanguageTech,
+  updateLanguageTech,
+  getLanguageTechVocabulary,
+  getTrendingLanguageTech
+};
---
+++ api/vocabData.js
@@ -0,0 +1,101 @@
+import client from '../utils/client';
+
+const endpoint = client.databaseURL;
+
+// GET VOCAB
+const getVocab = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }, // you technically do not need the options object for GET requests, but using it here for consistency
+  })
+    // updated getVocab function to handle a null value from the API
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+// DELETE VOCAB
+const deleteVocab = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    }, // you technically do not need the options object for GET requests, but using it here for consistency
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// GET SINGLE Vocab
+const getSingleVocab = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }, // you technically do not need the options object for GET requests, but using it here for consistency
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data)) // will resolve a single object
+    .catch(reject);
+});
+
+// CREATE Vocab
+const createVocab = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload)
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// UPDATE Vocab
+const updateVocab = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload)
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// FILTER vocabulary for WordOfTheMonth
+const vocabularyWordOfTheMonth = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary.json?orderBy="wordOfTheMonth"&equalTo=true`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// TODO: STRETCH...SEARCH vocabulary
+
+export {
+  getVocab,
+  createVocab,
+  vocabularyWordOfTheMonth,
+  deleteVocab,
+  getSingleVocab,
+  updateVocab
+};
