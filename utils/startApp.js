0fcc8fbf6c7473ef5394473529cfc238a907d9ab
Andre Phosarath <andrepho22@gmail.com>
2024-06-14 09:29:45 -0500

    fixed delete button - was passing object.values incorrectly into my domEvents

--- utils/startApp.js
+++ utils/startApp.js
@@ -10,12 +10,12 @@ import { showVocab } from '../pages/vocab';
 // import { showLanguageTech } from '../pages/langTech';
 
 const startApp = (user) => {
-  domBuilder(); // BUILD THE DOM
-  domEvents(user); // ADD THE EVENT LISTENTERS TO THE DOM
+  domBuilder(user); // BUILD THE DOM
   formEvents(user); // ADD FORM EVENT LISTENTERS TO THE DOM
   navBar(); // DYNAMICALLY ADD THE NAV
   logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
   navigationEvents(user); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
+  domEvents(user); // ADD THE EVENT LISTENTERS TO THE DOM
 
   // TODO: Put all Vocab on the DOM on App load
   getVocab(user.uid).then((vocab) => showVocab(vocab));
